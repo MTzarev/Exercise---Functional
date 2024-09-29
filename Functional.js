2bd8f00b89441c5a76b0950fef5249c0ddbf3028
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-29 23:15:01 +0300

    Update Functional.js

--- Functional.js
+++ Functional.js
@@ -3,16 +3,11 @@ let sum = 0;
 sum += a;
 
 function calc(b){
-    sum+=b
+    sum+=b;
     return calc;
 }
-
-
-
 calc.toString = ()=> sum;
-
 return calc;
-
 }
 console.log(functionalSum(1));
 console.log(`` + functionalSum(1)(6)(-3))
